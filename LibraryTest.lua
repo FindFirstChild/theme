0e42683f54a3995d48f33919bb02c168583b7e2c
Lunar Kevin <95857141+kevinYMHGmlg@users.noreply.github.com>
2023-04-05 20:33:03 +0700

    upd

--- LibraryTest.lua
+++ LibraryTest.lua
@@ -2,7 +2,7 @@ local RMA={}
 local Frames={}
 local Players=game:GetService'Players'
 local LocalPlayer=Players.LocalPlayer
-local CoreGui=LocalPlayer.PlayerGui --game.CoreGui
+local CoreGui=game.CoreGui
 local LPG=LocalPlayer.PlayerGui
 local MG=LPG:FindFirstChild'MainGui'or LPG:WaitForChild'MainGui'
 local AllBools={}
@@ -10,7 +10,7 @@ local function Create(Type)
 	if type(Type)~='string'then error("Argument of Create must be a string",2)end return function(dat)dat=dat or{}local obj=Instance.new(Type)local parent,ctor for k,v in next,dat do if type(k)=='string'then if k=='Parent'then parent=v continue end obj[k]=v elseif type(k)=='number'then if type(v)~='userdata'then error("Bad entry in Create body: Numeric keys must be paired with children, got a: "..type(v),2)end v.Parent=obj elseif type(k)=='table'and k.__eventname then if type(v)~='function'then error("Bad entry in Create body: Key `[Create.E\'"..k.__eventname.."\']` must have a function value, got: "..tostring(v),2)end obj[k.__eventname]:Connect(v)elseif k==Create then if type(v)~='function'then error("Bad entry in Create body: Key `[Create]` should be paired with a constructor function, got: "..tostring(v),2)elseif ctor then error("Bad entry in Create body: Only one constructor function is allowed",2)end ctor=v else error("Bad entry ("..tostring(k).." => "..tostring(v)..") in Create body",2)end end if ctor then ctor(obj)end if parent then obj.Parent=parent end return obj end
 end
 RMA.Create=Create
-function RMA.GetTables()
+function RMA:GetTables()
 	return Frames,AllBools
 end
 function RMA.CreateBool(Xame,Value)
@@ -18,7 +18,16 @@ function RMA.CreateBool(Xame,Value)
 	if Value then
 		Bool.Value=Value
 	end
-	table.insert(AllBools,Bool)
+	print(Xame)
+	AllBools[Xame]=Bool
+	Bool:GetPropertyChangedSignal'Value':Connect(function()
+		local str='local Conf={'
+		for n,b in next,select(2,RMA:GetTables())do
+			str..=('	%s=%s,\n'):format(n,b.Value)
+		end
+		str..='}\return Conf'
+		writefile('RMA2CONFIG.lua',str)
+	end)
 	return Bool
 end
 local function ClickSound()
@@ -43,25 +52,20 @@ local function Link(Button,Frame)
 	end)
 end
 RMA.Link=Link
-function RMA.CreateIcon(Xame,Xmage,Xosition,XnchorPoint)
+function RMA:CreateIcon(Xame,Xmage,Xosition,XnchorPoint)
 	local Label=Create'ImageButton'{Parent=MG,Name=Xame,Size=UDim2.new(0,40,0,40),Position=Xosition,BackgroundColor3=Color3.new(1,1,1),BorderSizePixel=0,Image=Xmage,AnchorPoint=XnchorPoint or Vector2.new(0,0)}
 	local UICorner=Create'UICorner'{CornerRadius=UDim.new(0,8),Parent=Label}
 	local Icon={}
 	function Icon:BindTo(Table)
 		Link(Label,Table.Gui)
 	end
-	function Icon.OnClick(Func,DisconnectAfterRuns)
-		local MB1=Label.MouseButton1Click
-		if DisconnectAfterRuns then
-			MB1:Once(Func)
-			return
-		end
-		MB1:Connect(Func)
+	function Icon.OnClick(Func)
+		Label.MouseButton1Click:Connect(Func)
 	end
 	Icon.Gui=Label
 	return Icon
 end
-function RMA.CreateButtonOld(Xame,Xext,Xosition,XnchorPoint)
+function RMA:CreateButtonOld(Xame,Xext,Xosition,XnchorPoint)
 	local Button=Create'TextButton'{Parent=MG,Name=Xame,Text=Xext,Size=UDim2.new(0,40,0,40),Position=Xosition,BackgroundColor3=Color3.new(1,1,1),BorderSizePixel=0,Font=Enum.Font.SourceSansSemibold,TextScaled=true,AnchorPoint=XnchorPoint or Vector2.new(0,0)}
 	Button.MouseButton1Click:Connect(ClickSound)
 	local UICorner=Create'UICorner'{Parent=Button,CornerRadius=UDim.new(0,8)}
@@ -69,18 +73,13 @@ function RMA.CreateButtonOld(Xame,Xext,Xosition,XnchorPoint)
 	function Buttons:BindTo(Frame)
 		Link(Button,Frame)
 	end
-	function Buttons.OnClick(Func,DisconnectAfterRuns)
-		local MB1=Button.MouseButton1Click
-		if DisconnectAfterRuns then
-			MB1:Once(Func)
-			return
-		end
-		MB1:Connect(Func)
+	function Buttons.OnClick(Func)
+		Button.MouseButton1Click:Connect(Func)
 	end
 	Buttons.Gui=Button
 	return Buttons
 end
-function RMA.CreateFrame(Xame,Heading)
+function RMA:CreateFrame(Xame,Heading)
 	local Label=Create'Frame'{Parent=MG,Name=Xame..'Frame',Size=UDim2.new(.4,0,.5,0),Position=UDim2.new(.3,0,.25,0),BackgroundColor3=Color3.fromRGB(34,34,34),BorderSizePixel=1,Style='Custom',Visible=false}
 	local LHeading=Create'TextLabel'{Parent=Label,AutomaticSize=Enum.AutomaticSize.None,BackgroundColor3=Color3.new(1,1,1),BackgroundTransparency=1,Name='Heading',Position=UDim2.new(0,0,.025,0),Size=UDim2.new(1,0,.1,0),Font=Enum.Font.SourceSansSemibold,LineHeight=1,RichText=false,Text=Heading,TextColor3=Color3.new(1,1,1),TextScaled=true,TextSize=14,TextWrapped=true,TextXAlignment=Enum.TextXAlignment.Center,TextYAlignment=Enum.TextYAlignment.Center}
 	local UICorner=Create'UICorner'{Parent=Label,CornerRadius=UDim.new(0,16)}
@@ -91,7 +90,7 @@ function RMA.CreateFrame(Xame,Heading)
 	end)
 	table.insert(Frames,Label)
 	local Panel={}
-	function Panel.CreateButton(Xame,Xext,Xosition)
+	function Panel:CreateButton(Xame,Xext,Xosition)
 		local Button=Create'TextButton'{Parent=Label,AutoButtonColor=true,BackgroundColor3=Color3.fromRGB(90,90,90),Modal=false,Name=Xame..'Button',Position=Xosition,Size=UDim2.new(.5,0,.075,0),Style=Enum.ButtonStyle.Custom,Font=Enum.Font.SourceSansSemibold,LineHeight=1,Text=Xext,TextColor3=Color3.new(1,1,1),TextScaled=true,TextSize=14,TextWrapped=true}
 		local UICorner=Create'UICorner'{CornerRadius=UDim.new(0,8),Parent=Button}
 		local Buttons={}
@@ -99,18 +98,13 @@ function RMA.CreateFrame(Xame,Heading)
 			local Frame=Table.Gui
 			Link(Button,Frame)
 		end
-		function Buttons.OnClick(Func,DisconnectAfterRuns)
-			local MB1=Button.MouseButton1Click
-			if DisconnectAfterRuns then
-				MB1:Once(Func)
-				return
-			end
-			MB1:Connect(Func)
+		function Buttons.OnClick(Func)
+			Button.MouseButton1Click:Connect(Func)
 		end
 		Buttons.Gui=Button
 		return Buttons
 	end
-	function Panel.CreateDK(Xame,Xosition,Xext,Bool,CodeOff,CodeOn)
+	function Panel:CreateDK(Xame,Xosition,Xext,Bool,CodeOff,CodeOn)
 		local Instances
 		local Frame=Create'Frame'{Parent=Label,AutomaticSize=Enum.AutomaticSize.None,BackgroundTransparency=1,Name=Xame,Position=Xosition,Size=UDim2.new(.9,0,.075,0),Style=Enum.FrameStyle.Custom}
 		local Value=Create'ImageButton'{Parent=Frame,AnchorPoint=Vector2.new(1,.5),BackgroundTransparency=1,Name='Value',Position=UDim2.new(1,0,.5,0),Size=UDim2.new(.1,40,1,40),Image='rbxassetid://1759563960',ImageColor3=Color3.fromRGB(90,90,90)}		
@@ -130,7 +124,7 @@ function RMA.CreateFrame(Xame,Heading)
 		DKs.Gui=Frame
 		return DKs
 	end
-	function Panel.CreateSub(Xext,Xize,XnchorPoint,Xosition,XextTransparency)
+	function Panel:CreateSub(Xext,Xize,XnchorPoint,Xosition,XextTransparency)
 		local Subheading=Create'TextLabel'{Parent=Label,BackgroundTransparency=1,Text=Xext,Size=Xize,AnchorPoint=XnchorPoint,Position=Xosition,Name='Subheading',Font=Enum.Font.SourceSansBold,TextColor3=Color3.new(1,1,1),TextScaled=true,TextXAlignment=Enum.TextXAlignment.Left,TextYAlignment=Enum.TextYAlignment.Center}
 		if XextTransparency then Subheading.TextTransparency=XextTransparency end
 		return Subheading
